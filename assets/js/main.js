419acb1758e452209230570974f0819c4a2ae402
mru24 <mru24@yahoo.com>
2019-04-30 18:28:28 +0100

    Static page build

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,33 @@
 $(document).ready(function() {
 
+  // PARALLAX EFFECT
+  $('[data-type="parallax"]').each(function() {
+    var obj = $(this);
+    $(window).scroll(function() {
+      var yPos = ($(window).scrollTop()) / obj.data('speed');
+      var coords = '50% ' + '-' + yPos + 'px';
+      obj.css({ backgroundPosition: coords });
+    })
+  })
+
+  // SCROLL TO ELEMENT ON PAGE
   $('a[href*="#"]').each(function() {
     $(this).click(function(e) {
         e.preventDefault();
         var link = $(this).attr('href');
-        console.log(link);
         $([document.documentElement, document.body]).animate({
           scrollTop: $(""+link).offset().top
         }, 500);
     });
   });
-  var $window = $(window);
-  $(window).scroll(function() {
-    console.log($(window).scrollTop());
-  })
 
+  $(window).scroll(function() {
+    // SCROLL TO TOP BUTTON
+    var scrollTop = $(window).scrollTop();
+    if(scrollTop > 400) {
+      $('a.scrollTop').addClass('showScrollTopButton');
+    } else {
+      $('a.scrollTop').removeClass('showScrollTopButton');
+    }
+  })
 });
